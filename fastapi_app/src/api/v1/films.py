5fff9a1fe282b0a51e8c5d4450c5be58cc80bf43
Alexander <cmrd-a@ya.ru>
2022-07-24 16:09:10 +0300

    Доработано получение списка фильмов.

--- fastapi_app/src/api/v1/films.py
+++ fastapi_app/src/api/v1/films.py
@@ -1,40 +1,45 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException, Query, Request
 
-from models.api_models import Film, FilmByPerson
-from services.film import FilmService, get_film_service
+from models.api_models import FilmFull, FilmsRated
+from services.film import FilmService, get_film_service, ApiSortOptions
 
 router = APIRouter()
 
 
-@router.get("/{film_id}", response_model=Film)
-async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
+@router.get("/{film_id}", response_model=FilmFull)
+async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)):
     film = await film_service.get_by_id(film_id)
     if not film:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
-    return Film(id=film.id, title=film.title)
+    return film
 
 
-@router.get("/", response_model=list[Film])
+@router.get("/", response_model=FilmsRated)
 async def films_list(
-    sort: str | None = Query(default=None),
+    request: Request,
+    sort: ApiSortOptions | None = Query(default=None),
     filter_genre: str | None = Query(default=None, alias="filter[genre]"),
     filter_person: str | None = Query(default=None, alias="filter[person]"),
     page_size: int = Query(default=50, alias="page[size]"),
     page_number: int = Query(default=1, alias="page[number]"),
     film_service: FilmService = Depends(get_film_service),
-) -> list[Film]:
-    films = await film_service.get_list(sort, filter_genre, filter_person, page_size, page_number)
+):
+    films = await film_service.get_list(request.query_params, sort, filter_genre, filter_person, page_size, page_number)
     if not films:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="films not found")
-    return [Film(id=film.id, title=film.title) for film in films]
+    return films
 
 
-@router.get("/search", response_model=list[FilmByPerson])
+@router.get("/search", response_model=FilmsRated)
 async def films_search(
     query: str | None = Query(default=None),
     page_size: int = Query(default=50, alias="page[size]"),
     page_number: int = Query(default=1, alias="page[number]"),
-) -> FilmByPerson:
-    pass
+    film_service: FilmService = Depends(get_film_service),
+):
+    films = await film_service.get_list(request.query_params, sort, filter_genre, filter_person, page_size, page_number)
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="films not found")
+    return films
--- fastapi_app/src/services/film.py
+++ fastapi_app/src/services/film.py
@@ -1,4 +1,4 @@
-import json
+from enum import Enum
 from functools import lru_cache
 
 from aioredis import Redis
@@ -7,9 +7,21 @@ from fastapi import Depends
 
 from db.elastic import get_elastic
 from db.redis import get_redis
-from models.es_models import Film
+from models.es_models import Film, FilmPaginated
 
-FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
+# FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
+FILM_CACHE_EXPIRE_IN_SECONDS = 5
+
+
+class ApiSortOptions(str, Enum):
+    imdb_asc = "imdb"
+    imdb_desc = "-imdb"
+
+
+ElasticSortMap = {
+    ApiSortOptions.imdb_asc: {"imdb_rating": {"order": "asc"}},
+    ApiSortOptions.imdb_desc: {"imdb_rating": {"order": "desc"}},
+}
 
 
 class FilmService:
@@ -29,22 +41,65 @@ class FilmService:
 
     async def get_list(
         self,
-        sort: str = None,
+        path,
+        sort: ApiSortOptions | None,
         filter_genre: str = None,
         filter_person: str = None,
         page_size: int = 50,
         page_number: int = 1,
-    ) -> list[Film]:
-        params = f"{sort}/{filter_genre}/{filter_person}/{page_size}/{page_number}"
-        films = await self._get_list_from_cache(params)
-        if not films:
-            resp = await self.elastic.search(index="movies", query={"match_all": {}}, size=page_size, sort=None)
-            hits = resp.body.get("hits", {}).get("hits", [])
-            films = [Film(**hit["_source"]) for hit in hits]
-            if not films:
-                return []
-            await self._put_list_to_cache(params, films)
-        return films
+    ) -> FilmPaginated | None:
+        redis_key = f"films/{path}"
+        data = await self._get_list_from_cache(redis_key)
+        if not data:
+            filters = []
+            if filter_genre:
+                filters.append(
+                    {"nested": {"path": "genres", "query": {"bool": {"must": {"term": {"genres.id": filter_genre}}}}}}
+                )
+            if filter_person:
+                filters.append(
+                    {
+                        "bool": {
+                            "should": [
+                                {
+                                    "nested": {
+                                        "path": "actors",
+                                        "query": {"bool": {"must": {"term": {"actors.id": filter_person}}}},
+                                    }
+                                },
+                                {
+                                    "nested": {
+                                        "path": "writers",
+                                        "query": {"bool": {"must": {"term": {"writers.id": filter_person}}}},
+                                    }
+                                },
+                                {
+                                    "nested": {
+                                        "path": "directors",
+                                        "query": {"bool": {"must": {"term": {"directors.id": filter_person}}}},
+                                    }
+                                },
+                            ]
+                        }
+                    }
+                )
+            resp = await self.elastic.search(
+                index="movies",
+                query={"bool": {"must": filters}},
+                sort=ElasticSortMap[sort] if sort else None,
+                size=page_size,
+                from_=(page_number - 1) * page_size if page_number > 1 else 0,
+            )
+            hits = resp.body.get("hits", {})
+            total = resp.body.get("hits", {})["total"]["value"]
+            _hits = hits.get("hits")
+            if not _hits:
+                return None
+            films = [Film(**hit["_source"]) for hit in _hits]
+            data = FilmPaginated(total=total, results=films)
+
+            await self._put_list_to_cache(redis_key, data)
+        return data
 
     async def _get_film_from_elastic(self, film_id: str) -> Film | None:
         try:
@@ -61,22 +116,21 @@ class FilmService:
         film = Film.parse_raw(data)
         return film
 
-    async def _get_list_from_cache(self, params: str) -> list[Film]:
+    async def _get_list_from_cache(self, params: str) -> FilmPaginated | None:
         data = await self.redis.get(params)
         if not data:
-            return []
+            return None
 
-        return [Film.parse_raw(raw_film) for raw_film in json.loads(data)]
+        return FilmPaginated.parse_raw(data)
 
     async def _put_film_to_cache(self, film: Film):
         await self.redis.set(film.id, film.json(), ex=FILM_CACHE_EXPIRE_IN_SECONDS)
 
-    async def _put_list_to_cache(self, params, films: list[Film]):
-        raw_films = [film.json() for film in films]
-        await self.redis.set(params, json.dumps(raw_films), ex=FILM_CACHE_EXPIRE_IN_SECONDS)
+    async def _put_list_to_cache(self, params, films: FilmPaginated):
+        await self.redis.set(params, films.json(), ex=FILM_CACHE_EXPIRE_IN_SECONDS)
 
 
-@lru_cache()
+@lru_cache
 def get_film_service(
     redis: Redis = Depends(get_redis),
     elastic: AsyncElasticsearch = Depends(get_elastic),
